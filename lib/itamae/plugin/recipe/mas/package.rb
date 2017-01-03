6e9da52ae7efa71e634467db6c2307fb99a0481c
takkanm <takkanm@gmail.com>
2017-01-04 08:45:25 +0900

    modify list
    allow `406056744 Evernote (6.10)` format such as `mas list`

--- lib/itamae/plugin/recipe/mas/package.rb
+++ lib/itamae/plugin/recipe/mas/package.rb
@@ -1,4 +1,5 @@
-node['mas']['app_ids'].each do |app_id|
+node['mas']['install_apps'].each do |app|
+  app_id = app.to_i
   app_name = `mas list | grep #{app_id}`.chomp.split[1..-1]
 
   execute "Install package: #{app_id} (#{app_name})" do
